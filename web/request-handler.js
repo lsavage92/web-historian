5940601aa8d5658808ad45b978ee7a0ae45c3ccb
Luke Savage <luke.savage92@gmail.com>
2015-07-23 21:42:04 -0500

    POST requests will now check if the web address already exists in the list, otherwise it will redirect to the loading page

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,14 +4,14 @@ var httpHelp = require('./http-helpers');
 var archive = require('../helpers/archive-helpers');
 var fetcher = require('../workers/htmlfetcher');
 var writer = require('./htmlwriter');
-var url = require('url');
+var urlParse = require('url');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
   writer.addUrl('example8.com');
 
   if (req.method === 'GET'){
-    var parts = url.parse(req.url);
+    var parts = urlParse.parse(req.url);
     var urlName = parts.pathname === '/' ? '/index.html' : parts.pathname;
     var moddedUrl = urlName.slice(1);
     //Once we have pathname
@@ -28,13 +28,34 @@ exports.handleRequest = function (req, res) {
         httpHelp.serveCb(err, data, res, 'text/html');
       });
     } else {
+      //Make sure to check that we aren't just waiting for archive.
       res.writeHead(404, exports.headers);
       res.end();
     }
   }
 
   if(req.method === 'POST'){
-
+    var url;
+    httpHelp.collectData(req, function(urlData){
+      url = urlData.split('=')[1];
+      //Is it in sites?
+      if (fetcher.checkUrl(url)){
+      //if so, check if archived.
+        archive.isUrlArchived(url, function(archived){
+          if (archived){
+          //if it is, display it.
+          //Still need helper function (downloadURLs)
+          } else {
+          //Otherwise, display loading.
+            httpHelp.sendResponse(res, '/loading.html');
+          }
+        });
+      } else {
+        //If not in sites, put it in sites
+        writer.addUrl(url);
+        httpHelp.sendResponse(res, '/loading.html');
+      }
+    });
   }
 
 };
